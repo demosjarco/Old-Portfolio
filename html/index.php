5fb7c146d52ef391bca959e25f73c91ffb65a077
demosjarco <demosjarco@users.noreply.github.com>
2021-04-08 15:00:48 -0700

    Test card

--- html/index.php
+++ html/index.php
@@ -40,18 +40,18 @@ $fileName = basename(__FILE__, '.php');
 		</footer>
 	</div>
 	<h2>School</h2>
+	<div class="card">
+		<img src="images/gcc.png" class="card-img-top" />
+		<div class="card-body">
+			<h5 class="card-title">GCC</h5>
+			<h6 class="card-subtitle text-muted">Information Security (AAS)</h6>
+			<p class="card-text">I have taken a variety of classes from server setup and management of linux and windows, Cisco networking, and security baseics to OS applications. I am also part of the school's Cyber Club.</p>
+		</div>
+		<div class="card-footer text-muted">January 2019 - December 2019</div>
+	</div>
 	<!--<div class="container">
 		<div class="row">
 			<div class="col-lg">
-				<div class="card">
-					<img src="images/gcc.png" class="card-img-top" />
-					<div class="card-body">
-						<h5 class="card-title">GCC</h5>
-						<h6 class="card-subtitle text-muted">Information Security (AAS)</h6>
-						<p class="card-text">I have taken a variety of classes from server setup and management of linux and windows, Cisco networking, and security baseics to OS applications. I am also part of the school's Cyber Club.</p>
-					</div>
-					<div class="card-footer text-muted">January 2019 - December 2019</div>
-				</div>
 			</div>
 			<div class="col-lg">
 				<div class="card">
