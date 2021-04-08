7cbf9ff121f22b812be20d6361ff475c8ca8e48b
demosjarco <demosjarco@users.noreply.github.com>
2021-04-08 15:59:09 -0700

    CSS test

--- html/index.php
+++ html/index.php
@@ -51,14 +51,14 @@ $fileName = basename(__FILE__, '.php');
 			<div class="tile gcc">
 				<div class="background"></div>
 				<div class="darken"></div>
-				<span class="singlemsg"></span><span class="title"></span><span class="subtitle"></span>
-				<footer></footer>
+				<span class="singlemsg">Message</span><span class="title">Title</span><span class="subtitle">Subtitle</span>
+				<footer>Footer</footer>
 			</div>
 			<div class="tile mcc">
 				<div class="background"></div>
 				<div class="darken"></div>
-				<span class="singlemsg"></span><span class="title"></span><span class="subtitle"></span>
-				<footer></footer>
+				<div class="singlemsg">Message</div><span class="title">Title</span><span class="subtitle">Subtitle</span>
+				<footer>Footer</footer>
 			</div>
 		</div>
 	</section>
