a54c636ee309af2c0392d170ec6222d1d2141ecd
demosjarco <demosjarco@users.noreply.github.com>
2021-07-17 21:57:47 -0700

    Updated url

--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,4 @@
 $(function() {
 	// Load background image async since big
-	// $('header div.background').css('background-image', 'url("../images/close-up-code-coding-239898.png")');
+	// $('header div.background').css('background-image', 'url("https://b2.demosjarco.dev/file/demosjarco-portfolio/images/close-up-code-coding-239898.png")');
 });
