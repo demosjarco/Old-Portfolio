df13d784ffad45d87d82b594af35b43bac860236
Victor <1209494+demosjarco@users.noreply.github.com>
2021-09-25 15:29:09 -0700

    ran mc

---
+++ html/mcmaps/overviewerConfig.js
@@ -0,0 +1,625 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Dedidated Minecwaft Sewvew",
+        "Dedidated Minecwaft Sewvew - nether",
+        "Legit",
+        "Legit - nether",
+        "Legit - end",
+        "Peyton Batmanning",
+        "Peyton Batmanning - nether",
+        "Peyton Batmanning - end"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1632608872",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Day UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "dms-day-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Dedidated Minecwaft Sewvew",
+            "last_rendertime": 1630517869,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                96,
+                -208
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -240,
+                96,
+                -208
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Caves UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "dms-caves-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Dedidated Minecwaft Sewvew",
+            "last_rendertime": 1630517869,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                96,
+                -208
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -240,
+                96,
+                -208
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Night UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "dms-night-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Dedidated Minecwaft Sewvew",
+            "last_rendertime": 1630517869,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                96,
+                -208
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -240,
+                96,
+                -208
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nether UL",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "dms-nether-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Dedidated Minecwaft Sewvew - nether",
+            "last_rendertime": 1620843062,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                96,
+                -208
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Minerals UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "dms-minerals-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Dedidated Minecwaft Sewvew",
+            "last_rendertime": 1630517869,
+            "imgextension": "webp",
+            "isOverlay": [
+                "dms-day-ul",
+                "dms-caves-ul",
+                "dms-night-ul"
+            ],
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                96,
+                -208
+            ],
+            "minZoom": 0,
+            "tilesets": [
+                "dms-day-ul",
+                "dms-caves-ul",
+                "dms-night-ul"
+            ],
+            "spawn": [
+                -240,
+                96,
+                -208
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Spawn UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "dms-spawn-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Dedidated Minecwaft Sewvew",
+            "last_rendertime": 1630517869,
+            "imgextension": "webp",
+            "isOverlay": [
+                "dms-day-ul",
+                "dms-caves-ul",
+                "dms-night-ul"
+            ],
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                96,
+                -208
+            ],
+            "minZoom": 0,
+            "tilesets": [
+                "dms-day-ul",
+                "dms-caves-ul",
+                "dms-night-ul"
+            ],
+            "spawn": [
+                -240,
+                96,
+                -208
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Day UL",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "l-day-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit",
+            "last_rendertime": 1623638225,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -57,
+                64,
+                182
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Caves UL",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "l-caves-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit",
+            "last_rendertime": 1623638225,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -57,
+                64,
+                182
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Night UL",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "l-night-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit",
+            "last_rendertime": 1623638225,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -57,
+                64,
+                182
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nether UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "l-nether-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit - nether",
+            "last_rendertime": 1404061238,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "End UL",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "l-end-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit - end",
+            "last_rendertime": 1404061238,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Minerals UL",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "l-minerals-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit",
+            "last_rendertime": 1623638225,
+            "imgextension": "webp",
+            "isOverlay": [
+                "l-day-ul",
+                "l-caves-ul",
+                "l-night-ul"
+            ],
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "tilesets": [
+                "l-day-ul",
+                "l-caves-ul",
+                "l-night-ul"
+            ],
+            "spawn": [
+                -57,
+                64,
+                182
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Spawn UL",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "l-spawn-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Legit",
+            "last_rendertime": 1623638225,
+            "imgextension": "webp",
+            "isOverlay": [
+                "l-day-ul",
+                "l-caves-ul",
+                "l-night-ul"
+            ],
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                64,
+                182
+            ],
+            "minZoom": 0,
+            "tilesets": [
+                "l-day-ul",
+                "l-caves-ul",
+                "l-night-ul"
+            ],
+            "spawn": [
+                -57,
+                64,
+                182
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Day UL",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "pb-day-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning",
+            "last_rendertime": 1630517873,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -95,
+                91,
+                164
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Caves UL",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "pb-caves-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning",
+            "last_rendertime": 1630517873,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -95,
+                91,
+                164
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Night UL",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "pb-night-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning",
+            "last_rendertime": 1630517873,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -95,
+                91,
+                164
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nether UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "pb-nether-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning - nether",
+            "last_rendertime": 1565917511,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "End UL",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "pb-end-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning - end",
+            "last_rendertime": 1566029079,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Minerals UL",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "pb-minerals-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning",
+            "last_rendertime": 1630517873,
+            "imgextension": "webp",
+            "isOverlay": [
+                "pb-day-ul",
+                "pb-caves-ul",
+                "pb-night-ul"
+            ],
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "tilesets": [
+                "pb-day-ul",
+                "pb-caves-ul",
+                "pb-night-ul"
+            ],
+            "spawn": [
+                -95,
+                91,
+                164
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Spawn UL",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "pb-spawn-ul",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Peyton Batmanning",
+            "last_rendertime": 1630517873,
+            "imgextension": "webp",
+            "isOverlay": [
+                "pb-day-ul",
+                "pb-caves-ul",
+                "pb-night-ul"
+            ],
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -95,
+                91,
+                164
+            ],
+            "minZoom": 0,
+            "tilesets": [
+                "pb-day-ul",
+                "pb-caves-ul",
+                "pb-night-ul"
+            ],
+            "spawn": [
+                -95,
+                91,
+                164
+            ],
+            "north_direction": 0
+        }
+    ]
+};
