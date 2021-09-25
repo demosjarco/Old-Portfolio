4160827c000e95c1c1241a1940e4ca864a488ec4
Victor <1209494+demosjarco@users.noreply.github.com>
2021-09-25 14:29:25 -0700

    Updated fontawesome

--- html/parts/preload.php
+++ html/parts/preload.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-header("Link: <https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css>; rel=preload; as=style; crossorigin", false);
+header("Link: <https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css>; rel=preload; as=style; crossorigin", false);
 header("Link: <https://fonts.googleapis.com/css?family=Lato:400,700|Roboto>; rel=preload; as=style; crossorigin", false);
 header("Link: </css/main.css>; as=style; rel=preload", false);
 header("Link: </css/light.css>; as=style; rel=preload", false);
