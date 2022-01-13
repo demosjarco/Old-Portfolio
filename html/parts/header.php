3c4ddf27f9ae0275cbff9c757184b19424c164c4
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-12 18:02:58 -0700

    Moved to zaraz

--- html/parts/header.php
+++ html/parts/header.php
@@ -1,6 +1,6 @@
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script> -->
-<script src="../js/darkMode.js"></script>
-<script src="../js/main.js"></script>
+<!-- <script src="../js/darkMode.js"></script> -->
+<!-- <script src="../js/main.js"></script> -->
 <?php
 $aboutClass = ($fileName == 'index') ? ' class="current"' : '';
 $contactClass = ($fileName == 'contact') ? ' class="current"' : '';
