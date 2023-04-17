7366fbe2267532bb2480bfdd3eca0b9817d0eb5f
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-04-17 10:40:50 -0700

    Added integrity hashes

--- html/parts/preload.php
+++ html/parts/preload.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
-header("Link: <https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css>; rel=preload; as=style; crossorigin", false);
+header("Link: <https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css>; rel=preload; as=style; crossorigin; integrity=sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==", false);
 header("Link: <https://fonts.googleapis.com/css?family=Lato:400,700|Roboto>; rel=preload; as=style; crossorigin", false);
 header("Link: </css/main.css>; as=style; rel=preload", false);
 header("Link: </css/light.css>; as=style; rel=preload", false);
 header("Link: </css/dark.css>; as=style; rel=preload", false);
 
-header("Link: <https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js>; as=script; rel=preload; crossorigin", false);
+header("Link: <https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js>; as=script; rel=preload; crossorigin; integrity=sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==", false);
 header("Link: </js/darkMode.js>; as=script; rel=preload", false);
 header("Link: </js/main.js>; as=script; rel=preload", false);
 
